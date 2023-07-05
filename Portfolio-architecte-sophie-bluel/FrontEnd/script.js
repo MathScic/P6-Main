a65d006b0a62fbe8147de3a67caf7977754aadf8
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-07-05 18:23:09 +0200

    rajout variable + tableau + function et avancer sur partie catégories

--- Portfolio-architecte-sophie-bluel/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/script.js
@@ -23,7 +23,22 @@ travaux.forEach((travail) => {
 // 2 foreach pour html et event listener pour chaque catégorie creer
 //+ methode filter pour filtrer le tableau et ensuite reconstruire le tableau
 
-let categories;
+let allTravaux = null;
+let objet = null;
+let appartement = null;
+let hotelEtRestaurant = null;
+let works = null;
+let categories = null;
+const objetCategoryId = 1;
+const appartementCategoryId = 2;
+const hotelEtRestaurantCategoryId = 3;
+const tableauCategories = [
+  { id: 1, name: "Objets" },
+  { id: 2, name: "Appartements" },
+  { id: 3, name: "Hotels et restaurants" },
+];
+
+let id;
 
 try {
   const response = await fetch("http://localhost:5678/api/categories");
@@ -32,22 +47,24 @@ try {
   console.log(error);
 }
 
-categories.forEach((allCategorie) => {
-  console.log(allCategorie.category);
-  let container = document.querySelector(".gallery");
-  const categoriesHtml = document.createElement("a");
-  categoriesHtml.innerText = "";
-  categoriesHtml.setAttribute("href", "");
-  const tableauCategorie = [(category = { id: 1, name: "objet" })];
-
-  console.log(tableauCategorie);
-
-  /**const filterTravaux = travaux.filter((work) => {
-    return work.categoryId === categoryId;
+const displayCategories = () => {
+  categories.forEach((category) => {
+    const container = document.querySelector(".gallery");
+    const categoriesHtml = document.createElement("a");
+    const listeCategories = document.createElement("ul");
+    categoriesHtml.innerText = "Ou apparait tu ?";
+    categoriesHtml.setAttribute("href", "");
+    listeCategories.innerHTML += `<li><a>${category.id[0].name["Objets"]}</a></li>`;
+    container.append(categoriesHtml);
+    //ajout ul li de Html de base
+    categoriesHtml.addEventListener("click", () => {
+      filtreTravauxParCategorie();
+    });
   });
-  console.log(categoryId);
+};
 
-  categoriesHtml.addEventListener("click", () => {});
-  **/
-  container.append(categoriesHtml);
-});
+const filtreTravauxParCategorie = (category, categoryId) => {
+  const filtreTravaux = travaux.filter((travail) => {
+    return tableauCategories.id === travail.categoryId;
+  });
+};
