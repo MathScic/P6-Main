031d0accb284763aed043548276fc4925f422a4d
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-07-25 13:05:21 +0200

    init

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/modal.js
@@ -0,0 +1,112 @@
+//Modal
+import { travaux, displayTravaux } from "./script.js";
+let modal = document.querySelector(".modal");
+const modalImg = modal.querySelector(".modal-img");
+let importImg;
+const containerImg = document.querySelector(".container_suppression");
+
+const openModal = async (e) => {
+  const maxImg = 11;
+  let counterImg = 0;
+
+  travaux.forEach((image) => {
+    console.log(image);
+    const imgElement = document.createElement("img");
+    imgElement.src = image.imageUrl;
+    console.log("image element", imgElement);
+    const lienSuppr = document.createElement("a"); // creer div avec img et lien pour adapter en css en absolute
+    lienSuppr.classList.add("lien_suppr");
+    lienSuppr.innerHTML = "<i class=fa-thin fa-trash-can></i>";
+    const div = document.createElement("div");
+    div.append(lienSuppr);
+    div.append(imgElement);
+    containerImg.append(div);
+
+    lienSuppr.addEventListener("click", async (e) => {
+      // suppression en local
+      containerImg.removeChild(div);
+
+      // suppression dans l'API
+      try {
+        const response = await fetch(
+          `http://localhost:5678/api/works/${image.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+
+        //
+
+        const allWorksWithoutTheDeletedOne = travaux.filter(
+          (work) => work.id !== image.id
+        );
+
+        displayTravaux(allWorksWithoutTheDeletedOne);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  });
+
+  modal.style.display = "flex"; //Sert a aficher la div de la modal
+  modal.setAttribute("aria-hidden", "false"); //indique modal affiché (pour malvoyant) au lecteur d'écran
+  modal.setAttribute("aria-modal", "true"); // indique que la div correspond a la modal
+};
+
+const closeModal = function (e) {
+  containerImg.innerHTML = "";
+  debugger;
+  modal.style.display = "none"; //masque la div de la modal
+  modal.setAttribute("aria-hidden", "true");
+};
+
+document
+  .querySelector(".js-modal")
+  .addEventListener("click", () => openModal());
+modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+
+//Pour close la modal avec escape
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closeModal(e);
+  }
+});
+
+/**Ajout photo **/
+const choixFichier = document.getElementById("choix-fichier");
+const imgPreview = document.getElementById("img_preview");
+let formData = new FormData();
+
+formData.append("userfiles", choixFichier.files);
+
+try {
+  const response = await fetch("", {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await response.json();
+  console.log(data);
+} catch (error) {
+  console.error("erreur survenue pendant requete");
+}
+
+choixFichier.addEventListener("change", function () {
+  getImgData();
+});
+
+function getImgData() {
+  const fichier = choixFichier.files[0];
+  if (fichier) {
+    const fichierReader = new FileReader();
+    fichierReader.readAsDataURL(fichier);
+    fichierReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = `<img src="` + this.result + `" />`;
+    });
+  }
+}
